a33ebd2a4da444cfb4ad760cf53f155afb4de17b
lg-epitech <laurent.gonzalez@epitech.eu>
2026-01-14 14:19:04 +0100

    fix: some deployment stuff

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: `https://${import.meta.env.VITE_DEPLOY_ADDRESS ?? "localhost"}/api`,
+  baseURL: `https://${import.meta.env.VITE_DEPLOY_ADDRESS ?? "localhost"}/api/auth`,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
