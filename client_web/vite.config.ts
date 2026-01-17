2b3a61c6ca36fcf08f4711b7c9b40d30f979ce15
Laurent Gonzalez <laurent.gonzalez@epitech.eu>
2026-01-17 18:54:05 +0100

    fix: frontend build

--- client_web/vite.config.ts
+++ client_web/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,13 @@ export default defineConfig({
     TanStackRouterVite(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@area/shared': path.resolve(__dirname, '../packages/shared'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@area/shared'],
+  },
 })
 
