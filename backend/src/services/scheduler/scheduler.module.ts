0c4c9c3d28afa08d2430c4c16ceb6fa90bef5ed9
William Jolivet <william.jolivet@epitech.eu>
2026-01-17 21:45:51 +0100

    feat: Introduce `OnActivationTrigger` to execute workflows immediately upon activation and integrate it into the `SchedulerModule`.

--- backend/src/services/scheduler/scheduler.module.ts
+++ backend/src/services/scheduler/scheduler.module.ts
@@ -1,21 +1,24 @@
 import { Module, OnModuleInit } from "@nestjs/common";
 import { CronTrigger } from "./triggers/cron.trigger";
+import { OnActivationTrigger } from "./triggers/on-activation.trigger";
 import { WorkflowsModule } from "../workflows/workflows.module";
 import { TriggerRegistryService } from "../registries/trigger-registry.service";
 
 @Module({
   imports: [WorkflowsModule],
-  providers: [CronTrigger],
-  exports: [CronTrigger],
+  providers: [CronTrigger, OnActivationTrigger],
+  exports: [CronTrigger, OnActivationTrigger],
 })
 export class SchedulerModule implements OnModuleInit {
   constructor(
     private readonly cronTrigger: CronTrigger,
+    private readonly onActivationTrigger: OnActivationTrigger,
     private readonly triggerRegistry: TriggerRegistryService,
-  ) {}
+  ) { }
 
   onModuleInit() {
     this.triggerRegistry.register(this.cronTrigger);
-    console.log("[SchedulerModule] Registered trigger: scheduler:cron");
+    this.triggerRegistry.register(this.onActivationTrigger);
+    console.log("[SchedulerModule] Registered triggers: scheduler:cron, scheduler:on-activation");
   }
 }
