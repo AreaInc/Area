37c5d56897c0b4eb67a52df5b58228ccfcaaad9d
Ubuntu <ubuntu@vps-dfe89ae3.vps.ovh.net>
2026-01-17 17:15:48 +0000

    fix: backend runtime imports

--- backend/src/services/telegram/telegram.module.ts
+++ backend/src/services/telegram/telegram.module.ts
@@ -1,8 +1,10 @@
 import { Module, OnModuleInit } from "@nestjs/common";
 import { ActionRegistryService } from "../registries/action-registry.service";
+import { WorkflowsModule } from "../workflows/workflows.module";
 import { SendTelegramMessageAction } from "./actions/send-message.action";
 
 @Module({
+  imports: [WorkflowsModule],
   providers: [SendTelegramMessageAction],
   exports: [SendTelegramMessageAction],
 })
@@ -10,7 +12,7 @@ export class TelegramModule implements OnModuleInit {
   constructor(
     private readonly sendTelegramMessageAction: SendTelegramMessageAction,
     private readonly actionRegistry: ActionRegistryService,
-  ) {}
+  ) { }
 
   onModuleInit() {
     this.actionRegistry.register(this.sendTelegramMessageAction);
